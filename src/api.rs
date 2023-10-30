a97a505a61b4931d799701c3411bd4bacb1ce4dd
Christian Roman <ricanontherun@gmail.com>
2023-10-30 09:01:25 -0400

    tests

--- src/api.rs
+++ src/api.rs
@@ -1,16 +1,27 @@
 pub mod tiny_json {
     use std::fs;
+    use std::fs::File;
+    use std::io::ErrorKind;
 
     #[derive(Debug)]
     pub struct Handle {
-        data_path: String,
+        path: String,
+        file: File
     }
 
     pub fn open(path: String) -> Result<Handle, String> {
-        match fs::create_dir(path.clone()) {
-            Err(_) => Err(String::from("failed to create file")),
-            _ => {
-                Ok(Handle { data_path: path.clone() })
+        match File::open(path.clone()) {
+            Ok(handle) => Ok(Handle{ path: path.clone(), file: handle}),
+            Err(e) => match e.kind() {
+                ErrorKind::NotFound =>
+                    match fs::create_dir(path.clone()) {
+                        Err(_) => Err(String::from("failed to create data directory")),
+                        Ok(_) => match File::open(path.clone()) {
+                            Ok(file) => Ok(Handle{file, path: path.clone()}),
+                            Err(_) => Err(String::from("hello"))
+                        }
+                    },
+                _ => Err(String::from("an error occurred attempting to open the data directory")),
             }
         }
     }
@@ -29,7 +40,12 @@ mod tests {
             assert_eq!(true, fs::remove_dir_all(test_data_path).is_ok())
         }
         assert_eq!(true, tiny_json::open(String::from(test_data_path)).is_ok());
-        assert_eq!(true, does_file_exist(test_data_path))
+        assert_eq!(true, does_file_exist(test_data_path));
+    }
+
+    #[test]
+    fn test_existing_directories_are_untouched() {
+
     }
 
     fn does_file_exist(filepath: &str) -> bool {
